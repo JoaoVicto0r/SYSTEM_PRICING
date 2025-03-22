15684369b0c8f6994c14528fb4b623dedad996da
Joao Victor <vektor5665@hottomail.com>
2025-03-22 09:20:32 -0300

    coloquei o typeORM cofigurei a criação de tabela, faltante configura o banco de dados ver a estrutura na pag appmodules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { User } from './user/user.entity';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+
+
+dotenv.config();
+
+const isOffline = process.env.DB_TYPE === 'sqlite';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: isOffline ? 'sqlite' : 'postgres',
+      database: isOffline ? process.env.SQLITE_DATABASE : process.env.POSTGRES_DB,
+      host: isOffline ? undefined : process.env.POSTGRES_HOST,
+      port: isOffline ? undefined : Number(process.env.POSTGRES_PORT),
+      username: isOffline ? undefined : process.env.POSTGRES_USER,
+      password: isOffline ? undefined : process.env.POSTGRES_PASSWORD,
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule
+  ],
+
+
+
+
+
+  
 })
 export class AppModule {}
+
